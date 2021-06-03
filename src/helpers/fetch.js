c8a406c9f6a2a1c03a5787facb8c64ad23594ea1
PabloM <molinapablod@gmail.com>
2021-06-03 13:24:00 -0300

    fecth call fixed

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -2,25 +2,19 @@ const baseUrl = process.env.REACT_APP_API_URL;
 
 
 export const fetchWithoutToken = (endpoint, data, method) => {
-     
+
     const url = `${baseUrl}/${endpoint}`;
 
     if (method === 'GET') {
-        
         return fetch(url)
-
-    }else{
-
-        return fetch({
-            method,
-            headers: {
-                'Content-type': 'application/json'
-            },
+    } else {
+        const requestOptions = {
+            method: method,
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(data)
-        })
+        };
 
-
+        return fetch(url, requestOptions)
     }
 
-     
- }
+}
